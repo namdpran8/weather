74475b35da17225b18f9940bb44113e77ae76251
Pranshu <namdeopranshu8@gmail.com>
2024-06-22 18:34:41 +0530

    ejs to html

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from "express"
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express()
 const port = 3000;
 
+
+app.use('/public', express.static('public'))
+
+
 app.get("/" , (req , res)=>{
-    res.render("index.ejs")
+    res.sendFile(__dirname + "/public/index.html")
 });
 
 app.listen(port, () => {
